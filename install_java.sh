67d3c285da9a207929d3e246c4cb452a379483ce
kunal0137 <kunal0137@gmail.com>
2024-11-14 12:09:20 -0500

    Update install_java.sh

--- install_java.sh
+++ install_java.sh
@@ -1,8 +1,7 @@
 arch=$(uname -m)
 if [[ $arch == x86_64* ]]; then
     echo "X64 Architecture"
-    wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu/bin/zulu8.78.0.19-ca-jdk8.0.412-linux_x64.tar.gz
-    tar --extract --file /tmp/zulujdk.tar.gz --directory "$JAVA_HOME" --strip-components 1
+    yum install java-1.8.0-openjdk-devel -y
 elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
     echo "ARM Architecture"
     wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu-embedded/bin/zulu8.76.0.17-ca-jdk8.0.402-linux_aarch64.tar.gz
