7f7b12bee9322176100badb00fe121f694e387f2
kunal0137 <kunal0137@gmail.com>
2025-07-14 11:24:09 -0400

    hardcode specific java version

--- install_java.sh
+++ install_java.sh
@@ -16,7 +16,7 @@
 arch=$(uname -m)
 if [[ $arch == x86_64* ]]; then
     echo "X64 Architecture"
-    wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu/bin/zulu${AZUL_ZULU_VERSION}-ca-jdk${JDK_VERSION}-linux_x64.tar.gz
+    wget -O /tmp/zulujdk.tar.gz https://cdn.azul.com/zulu/bin/zulu8.82.0.21-ca-jdk8.0.432-linux_x64.tar.gz
     tar --extract --file /tmp/zulujdk.tar.gz --directory "$JAVA_HOME" --strip-components 1
 elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
     echo "ARM Architecture"
